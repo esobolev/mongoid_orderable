a7e4886e1deeb4f71f86480221c5ca8a44cd6f53
Eugene Sobolev <sobolevea@ukr.net>
2015-06-01 15:32:09 +0300

    Update memory.rb

--- lib/mongoid_orderable/mongoid/contextual/memory.rb
+++ lib/mongoid_orderable/mongoid/contextual/memory.rb
@@ -5,6 +5,10 @@ module MongoidOrderable #:nodoc:
         def inc(* args)
           each do |document|
             document.inc *args
+            if document.updated_at.present?
+              document.updated_at = Time.now
+              document.save
+            end
           end
         end
       end
